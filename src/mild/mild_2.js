2bc875bf744d419bc9bb44dcc2d7a9188e3cfa57
johnmoore98 <johnkmoore98@gmail.com>
2021-10-28 11:11:47 -0400

    Surround destructuring in parentheses

--- src/mild/mild_2.js
+++ src/mild/mild_2.js
@@ -73,7 +73,7 @@ export function removeKey(object, key) {
  If only `removeKeyNonDestructive` was called, nothing would have changed.
  */
 export function removeKeyNonDestructive(object, key) {
-   var {key, obj} = object;
+   ({key, obj} = object);
    return obj;
 }
 
