6faa67a8a09c1396758bc356fb5b8f2d71285853
John Moore <johnkmoore98@gmail.com>
2021-11-09 14:33:00 -0500

    OH fix

--- src/medium/medium_2.js
+++ src/medium/medium_2.js
@@ -24,7 +24,7 @@ export const allCarStats = {
         city: mpg_data.reduce( (previous_val, curr_val) => curr_val.city_mpg + previous_val, 0) / mpg_data.length,
         highway: mpg_data.reduce( (previous_val, curr_val) => curr_val.highway_mpg + previous_val, 0) / mpg_data.length
     },
-    allYearStats: getStatistics(Array.from(mpg_data, x => x.year)),
+    allYearStats: getStatistics(mpg_data.map( x => x.year)),
     ratioHybrids: mpg_data.filter(x => x.hybrid == true).length / mpg_data.length
 };
 
